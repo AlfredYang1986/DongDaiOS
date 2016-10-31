e7f2d53352ffa835240968dd237a2a91e43f2a15
AlfredYang1986 <yangyuanpig@gmail.com>
2016-10-31 22:45:17 +0800

    环信登录周期性失败问题猜想（待测试）

--- BabySharing/Facade/LogicFacade/EM/LoginEM/AYLoginEMCommand.m
+++ BabySharing/Facade/LogicFacade/EM/LoginEM/AYLoginEMCommand.m
@@ -28,15 +28,11 @@ static NSString* const kAYEMDongdaCommonPassword = @"PassW0rd";
 
 - (void)performWithResult:(NSObject**)obj {
     NSString* current_user_id = (NSString*)*obj;
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        EMError *error = [[EMClient sharedClient] registerWithUsername:current_user_id password:kAYEMDongdaCommonPassword];
-//        NSLog(@"michauxs -- :%@",error);
-        if (error == nil || error.code == EMErrorUserAlreadyExist) {
-            error = [[EMClient sharedClient] loginWithUsername:current_user_id password:kAYEMDongdaCommonPassword];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (error == nil || error.code == EMErrorUserAlreadyLogin) {
-                    NSLog(@"环信: 登陆成功");
+
+    void (^registerSuccess)(void) = ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[EMClient sharedClient] asyncLoginWithUsername:current_user_id password:kAYEMDongdaCommonPassword success:^{
+                dispatch_async(dispatch_get_main_queue(), ^{
                     [[EMClient sharedClient].options setIsAutoLogin:YES];
                     [[EMClient sharedClient] dataMigrationTo3];
                     
@@ -49,23 +45,53 @@ static NSString* const kAYEMDongdaCommonPassword = @"PassW0rd";
                     
                     [notify setValue:[args copy] forKey:kAYNotifyArgsKey];
                     [((AYFacade*)EMCLIENT) performWithResult:&notify];
-                } else {
-                    NSLog(@"环信: 登陆失败");
-                }
-            });
+                });
+            } failure:^(EMError *aError) {
+                NSLog(@"环信: 登陆失败");
+            }];
+        });
+    };
+    
+    void (^rigisterFailed)(EMError* error) = ^(EMError* error) {
+        if (error == nil || error.code == EMErrorUserAlreadyExist) {
+            registerSuccess();
         } else {
             NSLog(@"环信: 注册失败");
-//            NSMutableDictionary* notify = [[NSMutableDictionary alloc]init];
-//            [notify setValue:kAYNotifyActionKeyNotify forKey:kAYNotifyActionKey];
-//            [notify setValue:kAYNotifyLoginEMFailedForSlowNetwork forKey:kAYNotifyFunctionKey];
-//            
-//            NSMutableDictionary* args = [[NSMutableDictionary alloc]init];
-//            [args setValue:current_user_id forKey:@"user_id"];
-//            
-//            [notify setValue:[args copy] forKey:kAYNotifyArgsKey];
-//            [((AYFacade*)EMCLIENT) performWithResult:&notify];
         }
-    });
+    };
+    
+    
+    [[EMClient sharedClient] asyncRegisterWithUsername:current_user_id password:kAYEMDongdaCommonPassword success:registerSuccess failure:rigisterFailed];
+    
+//     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//         EMError *error = [[EMClient sharedClient] registerWithUsername:current_user_id password:kAYEMDongdaCommonPassword];
+// //        NSLog(@"michauxs -- :%@",error);
+//         if (error == nil || error.code == EMErrorUserAlreadyExist) {
+//             error = [[EMClient sharedClient] loginWithUsername:current_user_id password:kAYEMDongdaCommonPassword];
+//             dispatch_async(dispatch_get_main_queue(), ^{
+//                 if (error == nil || error.code == EMErrorUserAlreadyLogin) {
+//                     NSLog(@"环信: 登陆成功");
+//                     [[EMClient sharedClient].options setIsAutoLogin:YES];
+//                     [[EMClient sharedClient] dataMigrationTo3];
+                    
+//                     NSMutableDictionary* notify = [[NSMutableDictionary alloc]init];
+//                     [notify setValue:kAYNotifyActionKeyNotify forKey:kAYNotifyActionKey];
+//                     [notify setValue:kAYNotifyLoginEMSuccess forKey:kAYNotifyFunctionKey];
+                    
+//                     NSMutableDictionary* args = [[NSMutableDictionary alloc]init];
+//                     [args setValue:current_user_id forKey:@"user_id"];
+                    
+//                     [notify setValue:[args copy] forKey:kAYNotifyArgsKey];
+//                     [((AYFacade*)EMCLIENT) performWithResult:&notify];
+//                 } else {
+//                     NSLog(@"环信: 登陆失败");
+//                 }
+//             });
+//         } else {
+//             NSLog(@"环信: 注册失败");
+
+//         }
+//     });
 }
 
 - (NSString*)getCommandType {
